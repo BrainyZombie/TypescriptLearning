8d62d41e86e0570ffc3a3e2580eb4501c0a3ffa7
ashir basu <ashirbasu@gmail.com>
2022-02-26 03:11:05 +0530

    02_types

---
+++ 02_types/types.js
@@ -0,0 +1,13 @@
+var today = new Date();
+console.log(today.getMonth());
+var person = {
+    age: 20
+};
+console.log(person.age);
+var Color = /** @class */ (function () {
+    function Color() {
+    }
+    return Color;
+}());
+var red = new Color();
+console.log(red);
---
+++ 02_types/types.ts
@@ -0,0 +1,13 @@
+const today = new Date();
+console.log(today.getMonth());
+
+const person = {
+    age: 20
+};
+console.log(person.age);
+
+class Color {
+
+}
+const red = new Color();
+console.log(red);
