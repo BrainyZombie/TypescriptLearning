e34701d54e7406abcbf601c8a8e7309d255cc042
ashir basu <ashirbasu@gmail.com>
2022-02-26 20:33:38 +0530

    constructors

--- 05_classes/classes.ts
+++ 05_classes/classes.ts
@@ -2,26 +2,30 @@ class Vehicle {
     // public drive(): void {
     //     console.log('Driving...');
     // }
+
+    constructor(public color: string){}
+    
     protected honk(): void {
         console.log('Honk!');
     }
 };
 
-class Car extends Vehicle {
-    private drive(): void {
-        console.log('Driving a car...');
-    }
+// class Car extends Vehicle {
+//     private drive(): void {
+//         console.log('Driving a car...');
+//     }
 
-    startDrivingProcess(): void {
-        this.drive();
-        this.honk();
-    }
-};
+//     startDrivingProcess(): void {
+//         this.drive();
+//         this.honk();
+//     }
+// };
 
-const vehicle = new Vehicle();
-const car = new Car();
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+// const car = new Car();
 
 // vehicle.drive();
 // vehicle.honk();
-car.startDrivingProcess();
+// car.startDrivingProcess();
 // car.honk();
