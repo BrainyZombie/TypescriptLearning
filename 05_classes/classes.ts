c4c182f6490912024fcdf617f9254097523e61d5
ashir basu <ashirbasu@gmail.com>
2022-02-26 20:26:57 +0530

    Modifiers

--- 05_classes/classes.ts
+++ 05_classes/classes.ts
@@ -1,22 +1,27 @@
 class Vehicle {
-    drive(): void {
-        console.log('Driving...');
-    }
-    honk(): void {
+    // public drive(): void {
+    //     console.log('Driving...');
+    // }
+    protected honk(): void {
         console.log('Honk!');
     }
 };
 
 class Car extends Vehicle {
-    drive(): void {
+    private drive(): void {
         console.log('Driving a car...');
+    }
+
+    startDrivingProcess(): void {
+        this.drive();
+        this.honk();
     }
 };
 
 const vehicle = new Vehicle();
 const car = new Car();
 
-vehicle.drive();
-vehicle.honk();
-car.drive();
-car.honk();
+// vehicle.drive();
+// vehicle.honk();
+car.startDrivingProcess();
+// car.honk();
