cbb45adfc54059e7364cda3682b14136a87cd698
ashir basu <ashirbasu@gmail.com>
2022-02-26 16:42:47 +0530

    Destructuring in functions

--- 02_annotations/functions.ts
+++ 02_annotations/functions.ts
@@ -9,3 +9,15 @@ function divide(a: number, b:number): number {
 const multiply = function(a: number, b:number): number{
     return a*b;
 }
+
+const forecast = {
+    date: new Date(),
+    weather: 'sunny'
+};
+
+const logWeather = ({date, weather}: {date: Date, weather: string}): void => {
+    console.log(date);
+    console.log(weather);
+}
+
+logWeather(forecast);
