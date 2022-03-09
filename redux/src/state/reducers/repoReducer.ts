0a36c5b5d4bc53c568cf3fda5623aa7a9bffaaee
ashir basu <ashirbasu@gmail.com>
2022-03-09 15:52:39 +0530

    reducer and actions

---
+++ redux/src/state/reducers/repoReducer.ts
@@ -0,0 +1,22 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+interface RepoState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepoState, action: Action): RepoState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPO:
+      return { data: [], loading: true, error: null };
+    case ActionType.SEARCH_REPO_SUCCESS:
+      return { data: action.payload, loading: false, error: null };
+    case ActionType.SEARCH_REPO_ERROR:
+      return { data: [], loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
